bc726610ca32ef2a4a271ba37744d1145f4a36d8
NBTaco <menandor119@gmail.com>
2024-12-12 11:48:14 +0100

    valtozok bevezetese

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+//változók bevezetése
+const fejlec1 = "Harc megnevezése" //fejlec 1. cellaja, string, fejlec1 változóba
+const fejlec2 = "Szembenálló felek" //fejlec 2. cellaja, string, fejlec2 változóba
+const fejlec3 = "Haderő" //fejlec 3. cellaja, string, fejlec3 változóba
+const harc1 = "Rákóczi szabadságharc" //1. rowSpan-es elem, a rowSpan-t később állítom be, string, harc1 változóba
+const elsosor1 = "Kuruc" //1. sor 1. cella, string, elsosor1 változóba
+const elsosor2 = "70.000" //1. sor 2. cella, string, elsosor2 változóba
+const masodiksor1 = "Labanc" //2. sor 1. cella, string, masodiksor1 változóba
+const masodiksor2 = "60.000" //2. sor 2. cella, string, masodiksor2 változóba
+const harc2 = "48-as szabadságharc" //2. rowSpan-es elem, a rowSpan-t később állítom be, string, harc2 változóba
+const harmadiksor1 = "Osztrák császárság(+ Orosz birodalom)" //3. sor 1. cella, string, harmadiksor1 változóba
+const harmadiksor2 = "170.000(+ 200.000)" //3. sor 2. cella, string, harmadiksor2 változóba
+const negyediksor1 = "Magyar királyság" //3. sor 1. cella, string, negyediksor1 változóba
+const negyediksor2 = "170.000" //4. sor 2. cella, string, negyediksor2 változóba
+const harc3 = "I. világháború" //2. rowSpan-es elem, a rowSpan-t később állítom be, string, harc3 változóba
+const otodiksor1 = "Antant" //5. sor 1. cella, string, otodiksor1 változóba
+const otodiksor2 = "43 millió" //5. sor 2. cella, string, otodiksor2 változóba
+const hatodiksor1 = "Központi hatalmak" //6. sor 1. cella, string, hatodiksor1 változóba
+const hatodiksor2 = "25 millió" //6. sor 2. cella, string, hatodiksor2 változóba
+const harc4 = "Bosworthi csata" // 4. cata a harc4 változóba, string
+const hetediksor1 = "Angolok(York + Lancester)" //7. sor 1. cella, string, hetediksor1 változóba
+const hetediksor2 = "15.000" //7. sor 2. cella, string, hetediksor2 változóba
+
 const table = document.createElement('table') // Létrehozzuk a table- elemet, createElement bementi paramétere string (ez lesz a html tag)
 document.body.appendChild(table) //a table elemet hozzáadjuk a body- hoz
 
@@ -9,15 +32,15 @@ const theadrow1 = document.createElement('tr') //létrehozunk egy tr- elemet, cr
 thead.appendChild(theadrow1) //a tr elemet hozzáadjuk a thead- hez
 
 const theadcell1 = document.createElement('td') //létrehozunk egy td- elemet, createElement bementi paramétere string (ez lesz a html tag)
-theadcell1.innerHTML = "Harc megnevezése" //a létrehozot td- nek beállítjuk a szövegét(innerHTML-t): Harc megnevezése-re, az innerHTML string
+theadcell1.innerHTML = fejlec1 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 theadrow1.appendChild(theadcell1) //a td elemet hozzáadjuk a tr- hez
  
 const theadcell2 = document.createElement('td') //létrehozunk egy td- elemet, createElement bementi paramétere string (ez lesz a html tag)
-theadcell2.innerHTML = "Szembenálló felek" //a létrehozot td- nek beállítjuk a szövegét(innerHTML-t): Szembenálló felek-re
+theadcell2.innerHTML = fejlec2 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 theadrow1.appendChild(theadcell2) //a td elemet hozzáadjuk a tr- hez
 
 const theadcell3 = document.createElement('td') //létrehozunk egy td- elemet, createElement bementi paramétere string (ez lesz a html tag)
-theadcell3.innerHTML = "Haderő" //a létrehozot td- nek beállítjuk a szövegét(innerHTML-t): Haderő-re
+theadcell3.innerHTML = fejlec3 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 theadrow1.appendChild(theadcell3) //a td elemet hozzáadjuk a tr- hez
 
 //tbody- megcsinálása, és az elemeinek hozzáadása
@@ -33,23 +56,23 @@ tbody.appendChild(tbodyrow2) //az 2. sort hozzáadjuk a tbody- hoz
 
 const row1td1 = document.createElement('td') //létrehozzuk az 1. sor 1. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
 row1td1.rowSpan = 2 //beállítjuk a rowSpant a cellára
-row1td1.innerHTML = "Rákóczi szabadságharc" //beállíjuk a cella szövegét (innerHTML-t):Rákóczi szabadságharc, az innerHTML string
+row1td1.innerHTML = harc1 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow1.appendChild(row1td1) //hozzáadjuk a cellát az 1. sorhoz
 
 const row1td2 = document.createElement('td') //létrehozzuk az 1. sor 2. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
-row1td2.innerHTML = "Kuruc" //beállíjuk a cella szövegét (innerHTML-t): Kuruc, az innerHTML string
+row1td2.innerHTML = elsosor1 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow1.appendChild(row1td2) //hozzáadjuk a cellát az 1. sorhoz
 
 const row1td3 = document.createElement('td') //létrehozzuk az 1. sor 3. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
-row1td3.innerHTML = "70.000" //beállíjuk a cella szövegét (innerHTML-t): 70.000, habár számot irunk be az innerHTML itt is string
+row1td3.innerHTML = elsosor2 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow1.appendChild(row1td3) //hozzáadjuk a cellát az 1. sorhoz
 
 const row2td1 = document.createElement('td') //létrehozzuk a 2. sor 1. celláját (td)- ez a középső cella lesz, mvel az első a rowSpan miatt már megvan, createElement bementi paramétere string (ez lesz a html tag)
-row2td1.innerHTML = "Labanc" //beállíjuk a cella szövegét (innerHTML-t):Labanc, az innerHTML string
+row2td1.innerHTML = masodiksor1 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow2.appendChild(row2td1) //hozzáadjuk a cellát a 2. sorhoz
 
 const row2td2 = document.createElement('td') //létrehozzuk a 2. sor 2. celláját (td)- ,utolsó cella, createElement bementi paramétere string (ez lesz a html tag)
-row2td2.innerHTML = "60.000" //beállíjuk a cella szövegét (innerHTML-t):60.000, habár számot irunk be az innerHTML itt is string
+row2td2.innerHTML = masodiksor2 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow2.appendChild(row2td2) //hozzáadjuk a cellát a 2. sorhoz
 
 // 2. sor
@@ -61,23 +84,23 @@ tbody.appendChild(tbodyrow4) //a 4. sort hozzáadjuk a tbody- hoz
 
 const row3td1 = document.createElement('td') //létrehozzuk az 3. sor 1. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
 row3td1.rowSpan = 2 //beállítjuk a rowSpant a cellára
-row3td1.innerHTML = "48-as szabadságharc" //beállíjuk a cella szövegét (innerHTML-t): 48-as szabadságharc, az innerHTML itt is string
+row3td1.innerHTML = harc2 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow3.appendChild(row3td1) //hozzáadjuk a cellát a 3. sorhoz
 
 const row3td2 = document.createElement('td') //létrehozzuk a 3. sor 2. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
-row3td2.innerHTML = "Osztrák császárság(+ Orosz birodalom)" //beállíjuk a cella szövegét (innerHTML-t): Osztrák császárság(+ Orosz birodalom), az innerHTML itt is string
+row3td2.innerHTML = harmadiksor1 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow3.appendChild(row3td2) //hozzáadjuk a cellát a 3. sorhoz
 
 const row3td3 = document.createElement('td') //létrehozzuk a 3. sor 3. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
-row3td3.innerHTML = "170.000(+ 200.000)" //beállíjuk a cella szövegét (innerHTML-t): 170.000(+ 200.000), az innerHTML itt is string
+row3td3.innerHTML = harmadiksor2 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow3.appendChild(row3td3) //hozzáadjuk a cellát a 3. sorhoz
 
 const row4td1 = document.createElement('td') //létrehozzuk a 4. sor 1. celláját (td)- ez a középső cella lesz, mvel az első a rowSpan miatt már megvan, createElement bementi paramétere string (ez lesz a html tag)
-row4td1.innerHTML = "Magyar királyság" //beállíjuk a cella szövegét (innerHTML-t): Magyar királyság, az innerHTML itt is string
+row4td1.innerHTML = negyediksor1 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow4.appendChild(row4td1) //hozzáadjuk a cellát a 4. sorhoz
 
 const row4td2 = document.createElement('td') //létrehozzuk a 4. sor 2. celláját (td)- ,utolsó cella, createElement bementi paramétere string (ez lesz a html tag)
-row4td2.innerHTML = "170.000" //beállíjuk a cella szövegét (innerHTML-t): 170.000, az innerHTML itt is string
+row4td2.innerHTML = negyediksor2 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow4.appendChild(row4td2) //hozzáadjuk a cellát a 4. sorhoz
 
 // 3. sor
@@ -89,23 +112,23 @@ tbody.appendChild(tbodyrow6)  //a 6. sort hozzáadjuk a tbody- hoz
 
 const row5td1 = document.createElement('td') //létrehozzuk az 5. sor 1. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
 row5td1.rowSpan = 2 //beállítjuk a rowSpant a cellára
-row5td1.innerHTML = "I. világháború" //beállíjuk a cella szövegét (innerHTML-t): I. világháború, az innerHTML itt is string
+row5td1.innerHTML = harc3 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow5.appendChild(row5td1) //hozzáadjuk a cellát az 5. sorhoz
 
 const row5td2 = document.createElement('td') //létrehozzuk az 5. sor 2. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
-row5td2.innerHTML = "Antant" //beállíjuk a cella szövegét (innerHTML-t): Antant, az innerHTML itt is string
+row5td2.innerHTML = otodiksor1 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow5.appendChild(row5td2) //hozzáadjuk a cellát az 5. sorhoz
 
 const row5td3 = document.createElement('td') //létrehozzuk az 5. sor 2. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
-row5td3.innerHTML = "43 millió" //beállíjuk a cella szövegét (innerHTML-t): 43 millió, az innerHTML itt is string
+row5td3.innerHTML = otodiksor2 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow5.appendChild(row5td3) //hozzáadjuk a cellát az 5. sorhoz
 
 const row6td1 = document.createElement('td') //létrehozzuk a 6. sor 1. celláját (td)- ez a középső cella lesz, mvel az első a rowSpan miatt már megvan, createElement bementi paramétere string (ez lesz a html tag)
-row6td1.innerHTML = "Központi Hatalmak" //beállíjuk a cella szövegét (innerHTML-t): Központi hatalmak, az innerHTML itt is string
+row6td1.innerHTML = hatodiksor1 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow6.appendChild(row6td1)//hozzáadjuk a cellát a 6. sorhoz
 
 const row6td2 = document.createElement('td') //létrehozzuk a 6. sor 2. celláját (td)- utolsó a sorban, createElement bementi paramétere string (ez lesz a html tag)
-row6td2.innerHTML = "25 millió" //beállíjuk a cella szövegét (innerHTML-t): 25 millió, az innerHTML itt is string
+row6td2.innerHTML = hatodiksor2 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow6.appendChild(row6td2) //hozzáadjuk a cellát a 6. sorhoz
 
 //4.sor
@@ -113,13 +136,13 @@ const tbodyrow7 = document.createElement('tr') //létrehozzuk a tbody 7. sorát,
 tbody.appendChild(tbodyrow7) //a 7. sort hozzáadjuk a tbody- hoz
 
 const row7td1 = document.createElement('td') //létrehozzuk a 7. sor 1. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
-row7td1.innerHTML = "Bosworthi csata" //beállíjuk a cella szövegét (innerHTML-t): Bosworthi csata, az innerHTML itt is string
+row7td1.innerHTML = harc4 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow7.appendChild(row7td1) //hozzáadjuk a cellát a 7. sorhoz
 
 const row7td2 = document.createElement('td') //létrehozzuk a 7. sor 2. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
-row7td2.innerHTML = "Angolok(York + Lancester)"  //beállíjuk a cella szövegét (innerHTML-t): Angolok(York + Lancester), az innerHTML itt is string
+row7td2.innerHTML = hetediksor1 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow7.appendChild(row7td2) //hozzáadjuk a cellát a 7. sorhoz
 
 const row7td3 = document.createElement('td') //létrehozzuk a 7. sor 3. celláját (td), createElement bementi paramétere string (ez lesz a html tag)
-row7td3.innerHTML = "15.000" //beállíjuk a cella szövegét (innerHTML-t): 15.000, az innerHTML itt is string
+row7td3.innerHTML = hetediksor2 //az innerHTML már nem egy string-el, hanem egy vááltozóval egyenlő (a változó string)
 tbodyrow7.appendChild(row7td3) //hozzáadjuk a cellát a 7. sorhoz
